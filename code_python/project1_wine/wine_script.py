2c79153299b30d0b95b2d16c55a8a59ec24cd840
domore <francisco.prado.moreno@gmail.com>
2021-08-12 17:02:18 +0200

    updated pdf, created table and some other output.

--- code_python/project1_wine/wine_script.py
+++ code_python/project1_wine/wine_script.py
@@ -50,4 +50,52 @@ while True:
 # plt.savefig(f'{path}/document_files/figs/wine_kde.pdf', format='pdf', bbox_inches='tight')
 plt.show()
 
+# Let us make an Appendix at the end with the description of each variable.
+description = {'fixed acidity': 'most acids involved with wine or fixed or nonvolatile (do not evaporate readily).',
+               'volatile acidity': 'the amount of acetic acid in wine, which at too high of levels can lead to an '
+                                   'unpleasant, vinegar taste.',
+               'citric acidity': 'found in small quantities, citric acid can add ``freshness" and flavor to wines.',
+               'residual sugar': 'the amount of sugar remaining after fermentation stops, it`s rare to find wines '
+                                 'with less than 1 gram/liter and wines with greater than 45 grams/liter are '
+                                 'considered sweet.',
+               'chlorides': 'the amount of salt in the wine.',
+               'free sulfur dioxide': 'the free form of SO2 exists in equilibrium between molecular SO2 (as a '
+                                      'dissolved gas) and bi-sulfate ion; it prevents microbial growth and the '
+                                      'oxidation of wine.',
+               'total sulfur dioxide': 'amount of free and bound forms of S02; in low concentrations, SO2 is mostly '
+                                       'undetectable in wine, but at free SO2 concentrations over 50 ppm, SO2 becomes '
+                                       'evident in the nose and taste of wine.',
+               'density': 'the density of water is close to that of water depending on the percent alcohol and sugar '
+                          'content.',
+               'pH': 'describes how acidic or basic a wine is on a scale from 0 (very acidic) to 14 (very basic); '
+                     'most wines are between 3-4 on the pH scale.',
+               'sulphates': 'a wine additive which can contribute to sulfur dioxide gas (S02) levels, which acts as '
+                            'an antimicrobial and antioxidant.',
+               'alcohol': 'the percent alcohol content of the wine.',
+               'quality': 'output variable (based on sensory data, score between 0 and 10).'
+               }
+description_df = pd.DataFrame(list(description.items()), columns=['Characteristic', 'Description'])
+# pd.options.display.max_colwidth = 400
+# print(description_df.to_latex(index=False, multirow=True),
+#      file=open(f'{path}/code_python/project1_wine/description_wine.tex', "a"))
+
+# End of Exploratory Analysis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
