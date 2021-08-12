681e7600e3642829ae6134f274d9ff8528121f49
domore <francisco.prado.moreno@gmail.com>
2021-08-12 14:09:38 +0200

    updated files, added pythontex folder to output and embed python code in the document.

--- code_python/project1_wine/wine_script.py
+++ code_python/project1_wine/wine_script.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 # Referencing folders and data names
 path = os.getcwd()
+# path = os.path.abspath(os.path.join(path, os.pardir))  # when in latex
 file_name = 'winequality-red.csv'
 path_file = f'{path}/code_python/project1_wine/data/{file_name}'
 
@@ -16,8 +17,7 @@ wine_data = pd.read_csv(path_file)
 # Exploratory analysis
 
 # Checking overall structure of the data
-wine_data.info()
-
+wine_data_info = wine_data.info()
 # Since there are no null or NAs we can proceed to analyse the data further
 
 # Using Pearson Correlation
@@ -26,7 +26,7 @@ corr_mat = wine_data.corr()
 ut_mask = np.triu(np.ones_like(corr_mat, dtype=bool))
 corr_heatmap = sns.heatmap(corr_mat, mask=ut_mask, annot=True, linewidths=.8, cmap='YlGnBu')
 corr_heatmap.set_title('Correlation Heatmap - Wine characteristics', fontdict={'fontsize': 21})
-plt.savefig(f'{path}/document_files/figs/wine_heatmap.pdf', format='pdf', bbox_inches='tight')
+# plt.savefig(f'{path}/document_files/figs/wine_heatmap.pdf', format='pdf', bbox_inches='tight')
 plt.show()
 
 # Subplots with KDE to check for clusters
@@ -40,12 +40,14 @@ while True:
     sns.kdeplot(ax=axes[row, column], x=wine_data.iloc[:, feature], y=wine_data.iloc[:, 11])
     if row == 2 and column == 2:
         sns.histplot(ax=axes[row, column+1], data=wine_data, y='quality', kde=True)
-        # fig.delaxes(ax=axes[row, column+1])
+        # fig.delaxes(ax=axes[row, column+1]) # if we want a blank output in the last subplot
         break
     feature += 1
     column += 1
     if column % 4 == 0:
         row += 1
         column = 0
-plt.savefig(f'{path}/document_files/figs/wine_kde.pdf', format='pdf', bbox_inches='tight')
+# plt.savefig(f'{path}/document_files/figs/wine_kde.pdf', format='pdf', bbox_inches='tight')
 plt.show()
+
+
